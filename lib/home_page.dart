bf3df6a809adbbd9feeca0bc6b0a1c38a13b393b
Kartikey singh <kartikeysingh4474@gmail.com>
2022-04-03 23:04:59 +0530

    Day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catalogapp/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,13 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(
-          child: Text("Welcome"),
-        )),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
